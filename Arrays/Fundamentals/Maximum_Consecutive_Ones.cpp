59b8e0e9e0b887e3f7d4314d2d83c63a7519a373
Divyansh Varshney <imdivarshney31@gmail.com>
2025-12-27 23:35:03 +0530

    Program for Maximum Consecutive Ones

---
+++ Arrays/Fundamentals/Maximum_Consecutive_Ones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt=0;
+        int max_cnt=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==1){
+                cnt+=1;
+                max_cnt=max(max_cnt, cnt);
+            }
+            else{
+                cnt=0;
+            }
+        }
+        return max_cnt;
+
+    }
+};
