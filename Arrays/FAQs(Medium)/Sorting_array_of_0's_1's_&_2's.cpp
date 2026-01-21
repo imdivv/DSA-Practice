5a1248fda38d089ecc7427b0b46a5787800d8276
Divyansh Varshney <imdivarshney31@gmail.com>
2026-01-21 22:17:29 +0530

    Program to Sort an Array of 0's 1's and 2's

---
+++ Arrays/FAQs(Medium)/Sorting_array_of_0's_1's_&_2's.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortZeroOneTwo(vector<int>& nums) {
+        int low=0;
+        int mid= 0;
+        int high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==2){
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+                
+            }
+            else{
+                mid++;
+            }
+        }
+        
+    }
+};
+
+int main() {
+    vector<int> nums = {0, 2, 1, 2, 0, 1};
+    Solution sol;
+    sol.sortZeroOneTwo(nums);
+    cout << "After sorting:" << endl;
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
